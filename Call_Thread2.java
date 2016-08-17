2bd6e2fdb7ce94bc271c79e6d0a0a5ae18e1d4d3
nishantambastha <nish.ambastha@gmail.com>
2016-08-18 01:05:14 +0530

    Call thread file

---
+++ Call_Thread2.java
@@ -0,0 +1,10 @@
+package Codes;
+
+public class Call_Thread2 {
+	public static void main(String args[]){
+		Thread.num = 2 ;
+		Thread.main(new String[0]);
+		System.out.println("test successful");
+	}
+}
+
